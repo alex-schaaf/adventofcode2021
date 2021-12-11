2a7b87fc8e56e29cb19db4c971134ce0337ada04
Alex <17277800+alex-schaaf@users.noreply.github.com>
2021-12-11 12:46:01 +0100

    🎅 solution day9.2

--- day09/main.py
+++ day09/main.py
@@ -26,9 +26,58 @@ for x in range(0, rows):
             isMinima &= n < data[x, y - 1]
         if y < cols - 1:
             isMinima &= n < data[x, y + 1]
-        
+
         if isMinima:
             minima.append(n)
             minima_xy.append((x, y))
 
 print(f"Part 1: {sum([m + 1 for m in minima])}")
+
+directions = [
+        (-1, 0),
+        (1, 0),
+        # (-1, -1),
+        # (1, 1),
+        # (-1, 1),
+        # (1, -1),
+        (0, -1),
+        (0, 1),
+    ]
+
+def bfs(grid: np.ndarray, i: int, j: int):
+    visited = set()
+    visit = {(i, j)}
+    basin = set()
+
+    while visit:
+        ci, cj = visit.pop()
+        height = grid[ci, cj]
+        # print(f"{(ci, cj)} -> {height}")
+        if height < 9:
+            basin.add((ci, cj))
+        visited.add((ci, cj))
+
+        for di, dj in directions:
+            if height == 9:
+                continue
+            ni, nj = ((ci + di, cj + dj))
+            if ni < 0 or nj < 0 or ni > grid.shape[0] - 1 or nj > grid.shape[1] - 1:
+                pass
+            elif (ni, nj) not in visited:
+                visit.add((ni, nj))
+
+    return len(basin)
+
+sizes = []
+for x, y in minima_xy[:]:
+    size = bfs(data, x, y)
+    sizes.append(size)
+    print(f"({x}, {y}) {size=}")
+
+product = 1
+for size in sorted(sizes)[-3:]:
+    product *= size
+
+print(f"Part 2: {product}")
+
+
